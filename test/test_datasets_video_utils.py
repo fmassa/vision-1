593f59d30454b86dcf8208452070f46179d016c9
Francisco Massa <fvsmassa@gmail.com>
2019-07-03 02:38:06 -0700

    More improvements

--- test/test_datasets_video_utils.py
+++ test/test_datasets_video_utils.py
@@ -68,6 +68,10 @@ class Tester(unittest.TestCase):
 
             video_clips = VideoClips(video_list, 6, 1)
             self.assertEqual(video_clips.num_clips(), 0 + (10 - 6 + 1) + (15 - 6 + 1))
+            for i, v_idx, c_idx in [(0, 1, 0), (4, 1, 4), (5, 2, 0), (6, 2, 1)]:
+                video_idx, clip_idx = video_clips.get_clip_location(i)
+                self.assertEqual(video_idx, v_idx)
+                self.assertEqual(clip_idx, c_idx)
 
 
 if __name__ == '__main__':
--- torchvision/datasets/video_utils.py
+++ torchvision/datasets/video_utils.py
@@ -19,6 +19,18 @@ def unfold(tensor, size, step, dilation):
 
 
 class VideoClips(object):
+    """
+    Given a list of video files, computes all consecutive subvideos of size
+    `clip_length_in_frames`, where the distance between each subvideo in the
+    same video is defined by `frames_between_clips`.
+
+    Creating this instance the first time is time-consuming, as it needs to
+    decode all the videos in `video_paths`. It is recommended that you
+    cache the results after instantiation of the class.
+
+    Recreating the clips for different clip lengths is fast, and can be done
+    with the `compute_clips` method.
+    """
     def __init__(self, video_paths, clip_length_in_frames=16, frames_between_clips=1):
         self.video_paths = video_paths
         self._compute_frame_pts()
@@ -26,11 +38,21 @@ class VideoClips(object):
 
     def _compute_frame_pts(self):
         self.video_pts = []
+        # TODO maybe paralellize this
         for video_file in self.video_paths:
             clips = read_video_timestamps(video_file)
             self.video_pts.append(torch.as_tensor(clips))
 
     def compute_clips(self, num_frames, step, dilation=1):
+        """
+        Compute all consecutive sequences of clips from video_pts.
+
+        Arguments:
+            num_frames (int): number of frames for the clip
+            step (int): distance between two clips
+            dilation (int): distance between two consecutive frames
+                in a clip
+        """
         self.num_frames = num_frames
         self.step = step
         self.dilation = dilation
@@ -48,9 +70,16 @@ class VideoClips(object):
         return len(self.video_paths)
 
     def num_clips(self):
+        """
+        Number of subclips that are available in the video list.
+        """
         return self.cumulative_sizes[-1]
 
     def get_clip_location(self, idx):
+        """
+        Converts a flattened representation of the indices into a video_idx, clip_idx
+        representation.
+        """
         video_idx = bisect.bisect_right(self.cumulative_sizes, idx)
         if video_idx == 0:
             clip_idx = idx
@@ -58,15 +87,22 @@ class VideoClips(object):
             clip_idx = idx - self.cumulative_sizes[video_idx - 1]
         return video_idx, clip_idx
 
-    def get_clip_pts_in_flat_index(self, idx):
-        video_idx, clip_idx = self.get_clip_location(idx)
-        return self.video_paths[video_idx], self.clips[video_idx][clip_idx], video_idx
-
-    def get_all_clip_pts_in_video(self, idx):
-        return self.video_paths[idx], self.clips[idx]
-
     def get_clip(self, idx):
-        video_path, clip_pts, video_idx = self.get_clip_pts_in_flat_index(idx)
+        """
+        Gets a subclip from a list of videos.
+
+        Arguments:
+            idx (int): index of the subclip. Must be between 0 and num_clips().
+
+        Returns:
+            video (Tensor)
+            audio (Tensor)
+            info (Dict)
+            video_idx (int): index of the video in `video_paths`
+        """
+        video_idx, clip_idx = self.get_clip_location(idx)
+        video_path = self.video_paths[video_idx]
+        clip_pts = self.clips[video_idx][clip_idx]
         video, audio, info = read_video(video_path, clip_pts[0].item(), clip_pts[-1].item())
         video = video[::self.dilation]
         # TODO change video_fps in info?
